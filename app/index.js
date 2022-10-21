bc2826519fb2896adbca856a30c8dd80a395d68a
joaoCeilandia <joaovitordmrosa@hotmail.com>
2022-10-20 22:56:49 -0300

    add port

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import app from './app';
 
-app.listen(3001, () => {
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
   // eslint-disable-next-line import/extensions
-  console.log('API running on localhost:3001');
+  console.log(`API running on ${port}`);
 });
