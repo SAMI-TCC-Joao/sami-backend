367d260ddad551cfbe513b8c3adeb74f6360f763
joaoCeilandia <joaovitordmrosa@hotmail.com>
2023-02-06 18:43:42 -0300

    teste console

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -43,6 +43,7 @@ export class FormController {
   @Get('template')
   @ApiOperation({ summary: 'Get all template forms' })
   findAllTemplates() {
+    console.log('ta entrando no template');
     return this.formService.findAllTemplates();
   }
 
@@ -55,6 +56,7 @@ export class FormController {
     @LoggedUser() userLogged: User,
     @Query('withIndicator') withIndicator: boolean,
   ) {
+    console.log('ta entrando no get geral');
     isAllowedOrIsMe(userType.admin.value, userLogged, userLogged.id);
     return this.formService.findAll(userLogged, `${withIndicator}`);
   }
